9e28a26972eacd339ea72f1f1308e4141a55700b
David Batista <xmedavid@users.noreply.github.com>
2025-04-02 22:59:05 +0200

    nvim init

---
+++ .config/nvim/lua/core/options.lua
@@ -0,0 +1,29 @@
+local opt = vim.opt
+
+opt.number = true
+opt.relativenumber = true
+
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.softtabstop = 4
+opt.smartindent = true
+opt.autoindent = true
+opt.wrap = false
+
+opt.clipboard = "unnamedplus"
+
+opt.scrolloff = 8
+opt.sidescrolloff = 8
+
+opt.splitright = true
+opt.splitbelow = true
+
+opt.swapfile = false
+opt.backup = false
+opt.undofile = true
+opt.encoding = "utf-8"
+
+opt.ignorecase = true
+opt.smartcase = true
+opt.hlsearch = true
+opt.incsearch = true
